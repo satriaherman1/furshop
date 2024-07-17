44b352dd471dec1c9acc73e0a37cc056aa6b12f1
satriaherman1 <muhammadsatriaherman@gmail.com>
2024-07-17 10:12:28 +0700

    change db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn' => '',
-	'hostname' => 'SG-clammy-yogurt-9545-9546-mysql-master.servers.mongodirector.com',
-	'username' => 'sgroot',
-	'password' => 'J8RhD1r.2LOPG5nz',
-	'database' => 'furshop',
+	'hostname' => 'sql12.freemysqlhosting.net',
+	'username' => 'sql12719907',
+	'password' => 'z1ySKPIwzL',
+	'database' => 'sql12719907',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
